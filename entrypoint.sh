f98c8695ce91cde653c6986f6c499d0b6b51178e
aramirol <56970108+aramirol@users.noreply.github.com>
2021-10-04 15:44:33 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-git remote add gitlab https://${INPUT_GITLAB_USERNAME}:${INPUT_GITLAB_TOKEN}@${INPUT_GITLAB_URL#https://}
+git remote add gitlab https://${INPUT_REPO_DEST_USERNAME}:${INPUT_REPO_DEST_TOKEN}@${INPUT_REPO_DEST_URL#https://}
 
 case "${GITHUB_EVENT_NAME}" in
     push)
